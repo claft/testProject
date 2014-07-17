07637e7e422adf58853e62285e04d062fc36b334
k.kinoshita <k.kinoshita@pastoraldog.com>
2014-07-17 09:42:40 +0900

    test project

---
+++ Assets/Editor/myUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace MyUnityTest
+{
+	class SampleTests
+	{
+		[TestCase("fizz")]
+		public void constructor(string test)
+		{
+			Fizzbuzz fb = new Fizzbuzz (test);
+			Assert.AreEqual (fb.Ret, test);
+		}
+	}
+}
---
+++ Assets/Script/fizzbuzz.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fizzbuzz {
+	public string Ret { get; private set; }
+
+	public Fizzbuzz(string test)
+	{
+		Ret = test;
+	}
+}
