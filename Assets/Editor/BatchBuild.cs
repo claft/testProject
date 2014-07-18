fba54b675737f023a4e78084fdc2c80a33f5ae3c
k.kinoshita <k.kinoshita@pastoraldog.com>
2014-07-18 22:39:01 +0900

    test3

--- Assets/Editor/BatchBuild.cs
+++ Assets/Editor/BatchBuild.cs
@@ -1,17 +1,21 @@
-﻿[UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
-public static void BuildProjectAllSceneAndroid() {
-	EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
-	string[] allScene = new string[EditorBuildSettings.scenes.Length];
-	int i = 0;
-	foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
-		allScene[i] = scene.path;
-		i++;
-	}	
-	PlayerSettings.bundleIdentifier = "jp.co.hoge.hoge";
-	PlayerSettings.statusBarHidden = true;
-	BuildPipeline.BuildPlayer( allScene,
+﻿public class BatchBuild
+{
+		[UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+		public static void BuildProjectAllSceneAndroid ()
+		{
+				EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.Android);
+				string[] allScene = new string[EditorBuildSettings.scenes.Length];
+				int i = 0;
+				foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+						allScene [i] = scene.path;
+						i++;
+				}	
+				PlayerSettings.bundleIdentifier = "jp.co.hoge.hoge";
+				PlayerSettings.statusBarHidden = true;
+				BuildPipeline.BuildPlayer (allScene,
 	                          "hoge.apk",
 	                          BuildTarget.Android,
 	                          BuildOptions.None
-	                          );
+				);
+		}
 }
